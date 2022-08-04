7391d7b379fb6d9af0153d9f97d04b2d0f287cf2
Fabrizio Rossi <fabry_98@hotmail.it>
2022-08-04 14:40:44 +0200

    Added Event CAE

--- train.py
+++ train.py
@@ -119,3 +119,57 @@ def train_transformer(
                 int(elapsed_time),
             )
         )
+
+def train_autoencoder(
+    model: torch.nn.Module,
+    device: str,
+    train_ds: torch.utils.data.Dataset,
+    params: Dict,
+):
+
+    model.to(device)
+
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=params["learning_rate"])
+
+    model.train()
+    for epoch in range(params["n_epochs"]):
+        start_epoch_time = time.time()
+        batch_losses = []
+        print(
+            "Epoch {}/{} Step {}/{} Loss: {:.5f}".format(
+                epoch + 1, params["n_epochs"], 1, len(train_ds), 0
+            ),
+            end="",
+        )
+        for step, sample in enumerate(train_ds):
+            (_, batch), _ = sample
+            # batch shape (bins, height, width)
+            batch = torch.unsqueeze(torch.from_numpy(batch), 1).to(device)
+
+            generated_batch = model(batch)
+
+            loss: torch.Tensor = criterion(batch, generated_batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            batch_losses.append(loss.cpu().detach())
+
+            print(
+                "\rEpoch {}/{} Step {}/{} Loss: {:.5f}".format(
+                    epoch + 1, params["n_epochs"], step + 1, len(train_ds), loss
+                ),
+                end="",
+            )
+        elapsed_time = time.time() - start_epoch_time
+        print(
+            "\rEpoch {}/{} Step {}/{} Mean Loss: {:.5f} Elapsed Seconds: {}s".format(
+                epoch + 1,
+                params["n_epochs"],
+                step + 1,
+                len(train_ds),
+                np.mean(batch_losses),
+                int(elapsed_time),
+            )
+        )
