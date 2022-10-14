70d005fb95841e18f0d11928fca362c783b3a323
Fabrizio Rossi <fabry_98@hotmail.it>
2022-10-14 19:09:47 +0200

    Run Colab: Large - 1e-4 lr, 32-64 conv, 1e-3 fl, fix

--- models/transformer.py
+++ models/transformer.py
@@ -384,6 +384,7 @@ class VisionTransformerConv(pl.LightningModule):
         heads: int,
         layers_number: int,
         learning_rate: float,
+        feature_loss_weight: int,
     ):
         super().__init__()
         self.save_hyperparameters()
@@ -392,21 +393,22 @@ class VisionTransformerConv(pl.LightningModule):
         self.bins, self.h, self.w = self.input_shape
         self.p_h, self.p_w = patch_size
         self.lr = learning_rate
+        self.feature_loss_weight = feature_loss_weight
 
         self.token_dim = self.p_w * self.p_h
 
         self.conv_encoder = torch.nn.Sequential(
-            torch.nn.Conv2d(10, 16, 3, padding="same"),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm2d(16),
-            torch.nn.Conv2d(16, 16, 3, padding="same"),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm2d(16),
-            torch.nn.MaxPool2d(2),
-            torch.nn.Conv2d(16, 32, 3, padding="same"),
+            torch.nn.Conv2d(10, 32, 3, padding="same"),
             torch.nn.ReLU(),
             torch.nn.BatchNorm2d(32),
             torch.nn.Conv2d(32, 32, 3, padding="same"),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(32, 64, 3, padding="same"),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.Conv2d(64, 64, 3, padding="same"),
             torch.nn.ReLU(),
         )
 
@@ -419,24 +421,24 @@ class VisionTransformerConv(pl.LightningModule):
         self.enc = torch.nn.TransformerEncoder(enc_layer, layers_number)
 
         self.conv_decoder = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(64, 64, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ConvTranspose2d(64, 32, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ConvTranspose2d(32, 32, 2, 2, padding=0),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(32),
             torch.nn.ConvTranspose2d(32, 32, 3, padding=1),
             torch.nn.ReLU(),
             torch.nn.BatchNorm2d(32),
-            torch.nn.ConvTranspose2d(32, 16, 3, padding=1),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm2d(16),
-            torch.nn.ConvTranspose2d(16, 16, 2, 2, padding=0),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm2d(16),
-            torch.nn.ConvTranspose2d(16, 16, 3, padding=1),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm2d(16),
-            torch.nn.Conv2d(16, 3, 3, padding=1),
+            torch.nn.Conv2d(32, 3, 3, padding=1),
             torch.nn.Sigmoid(),
         )
 
         self.lpips = torchmetrics.image.lpip.LearnedPerceptualImagePatchSimilarity(
-            weights=VGG16_Weights.DEFAULT
+            net_type="vgg"
         )
         self.ssim = torchmetrics.functional.structural_similarity_index_measure
         self.mse = torchmetrics.functional.mean_squared_error
@@ -486,7 +488,7 @@ class VisionTransformerConv(pl.LightningModule):
         image_loss = criterion(model_images, y)
         features_loss = criterion(pre, post)
 
-        loss = image_loss + 1e-2 * features_loss
+        loss = image_loss + self.feature_loss_weight * features_loss
 
         self.log("train_image_loss", image_loss)
         self.log("train_features_loss", features_loss)
@@ -512,7 +514,7 @@ class VisionTransformerConv(pl.LightningModule):
         image_loss = criterion(model_images, y)
         features_loss = criterion(pre, post)
 
-        loss = image_loss + 1e-2 * features_loss
+        loss = image_loss + self.feature_loss_weight * features_loss
 
         self.log("val_image_loss", image_loss)
         self.log("val_features_loss", features_loss)
