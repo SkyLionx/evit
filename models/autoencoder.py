715729480030013ade23391ecea90c5ed2294420
Fabrizio Rossi <fabry_98@hotmail.it>
2022-10-31 10:10:54 +0100

    Reworked AutoEncoder, Run Colab: Large - Event AE

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,16 +1,15 @@
-from typing import Tuple
+import pytorch_lightning as pl
 import torch
 
 
 class EventEncoder(torch.nn.Module):
-    def __init__(self, n_filters: list):
+    def __init__(self, input_features: int, n_filters: list):
         super().__init__()
 
         self.enc_blocks = torch.nn.ModuleList()
-        for i in range(len(n_filters)):
 
-            if i == 0:
-                in_features = 1
+        in_features = input_features
+        for i in range(len(n_filters)):
             out_features = n_filters[i]
 
             block = torch.nn.Sequential(
@@ -26,31 +25,27 @@ class EventEncoder(torch.nn.Module):
 
             in_features = out_features
 
-        self.last_conv = torch.nn.Conv2d(out_features, 1, (3, 3), padding="same")
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         for block in self.enc_blocks:
             x = block(x)
-        x = self.last_conv(x)
-        return x.flatten(start_dim=1)
+        return x
 
 
 class EventDecoder(torch.nn.Module):
-    def __init__(self, output_shape: Tuple[int, int, int], n_filters: list):
+    def __init__(self, input_features: int, n_filters: list):
         super().__init__()
-        self.output_shape = output_shape
-        self.dec_blocks = torch.nn.ModuleList()
-        for i in range(len(n_filters)):
 
-            if i == 0:
-                in_features = 1
+        self.dec_blocks = torch.nn.ModuleList()
+
+        in_features = input_features
+        for i in range(len(n_filters)):
             out_features = n_filters[i]
 
             block = torch.nn.Sequential(
                 torch.nn.ConvTranspose2d(in_features, out_features, (2, 2), stride=2),
                 torch.nn.BatchNorm2d(out_features),
                 torch.nn.ReLU(),
-                torch.nn.Conv2d(out_features, out_features, (3, 3), padding=1),
+                torch.nn.Conv2d(out_features, out_features, (3, 3), padding="same"),
                 torch.nn.BatchNorm2d(out_features),
                 torch.nn.ReLU(),
             )
@@ -58,27 +53,52 @@ class EventDecoder(torch.nn.Module):
 
             in_features = out_features
 
-        self.last_conv = torch.nn.Conv2d(
-            out_features, output_shape[-1], (3, 3), padding="same"
-        )
+        self.last_conv = torch.nn.Conv2d(out_features, 1, (3, 3), padding="same")
         self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        width = self.output_shape[0] // (2 ** len(self.dec_blocks))
-        height = self.output_shape[1] // (2 ** len(self.dec_blocks))
-        x = x.reshape(-1, 1, height, width)
         for block in self.dec_blocks:
             x = block(x)
+
         x = self.last_conv(x)
-        return self.sigmoid(x)
+        x = self.sigmoid(x)
+
+        return x
 
 
-class EventAutoEncoder(torch.nn.Module):
-    def __init__(self, encoder: torch.nn.Module, decoder: torch.nn.Module):
+class EventAutoEncoder(pl.LightningModule):
+    def __init__(self, encoder: torch.nn.Module, decoder: torch.nn.Module, lr: float):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
+        self.lr = lr
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         code = self.encoder(x)
         return self.decoder(code)
+
+    def training_step(self, train_batch: torch.Tensor, batch_idx):
+        X, _ = train_batch
+        X = X.flatten(start_dim=0, end_dim=1).unsqueeze(1)
+        X_hat = self(X)
+
+        criterion = torch.nn.MSELoss()
+        loss = criterion(X, X_hat)
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, valid_batch: torch.Tensor, batch_idx):
+        X, _ = valid_batch
+        X = X.flatten(start_dim=0, end_dim=1).unsqueeze(1)
+        X_hat = self(X)
+
+        criterion = torch.nn.MSELoss()
+        loss = criterion(X, X_hat)
+        self.log("val_loss", loss)
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
