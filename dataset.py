0f480576e3db3ec4c1600b75bda34af1053b8efb
Fabrizio Rossi <fabry_98@hotmail.it>
2022-11-05 11:52:26 +0100

    Added different types of events normalization

--- dataset.py
+++ dataset.py
@@ -18,15 +18,33 @@ class CustomDataset(abc.ABC, torch.utils.data.Dataset):
         crop_size: Tuple[int, int] = None,
         sequences: List[str] = [],
         convert_to_bw: bool = False,
-        normalize_events: bool = False,
+        events_normalization: str = None,
     ):
+        """
+        Custom dataset that offers some utilities.
+
+        Args:
+            dataset_path (str): parent folder that contains one folder for each sequence.
+            limit (int, optional): load only this number of samples. If None, no limit is imposed. Defaults to None.
+            preload_to_RAM (bool, optional): preload samples to RAM, preprocessing them. Defaults to False.
+            crop_size (Tuple[int, int], optional): crop events and images to this size (w x h). Defaults to None.
+            sequences (List[str], optional): load only this list of sequences (folders). If empty list, every sequence will be loaded. Defaults to [].
+            convert_to_bw (bool, optional): convert the ground truth images to black and white. Defaults to False.
+            events_normalization (str, optional): normalize the events after being cropped.
+            If None, no normalization will be perfomed, otherwise it should be one of the following strings:
+            "min_max_0_1": min max normalization with values in the range [0, 1] (the max assumed is 255 and the min -255).
+            "min_max_-1_1": min max normalization with values in the range [-1, 1] (the max assumed is 255 and the min -255).
+            "z_score": z score normalization making values with mean 0 and std 1.
+            "z_score_non_zero": z score normalization making values with mean 0 and std 1 only for values different from zero.
+            Defaults to None.
+        """
         super().__init__()
         self.dataset_path = dataset_path
         self.limit = limit
         self.preload_to_RAM = preload_to_RAM
         self.crop_size = crop_size
         self.convert_to_bw = convert_to_bw
-        self.normalize_events = normalize_events
+        self.events_normalization = events_normalization
 
         self.files_list = []
         self.data = []
@@ -36,14 +54,18 @@ class CustomDataset(abc.ABC, torch.utils.data.Dataset):
 
         for sequence_folder in sorted(sequences):
             seq_path = os.path.join(dataset_path, sequence_folder)
+
+            # Support an additional folder called batches inside the sequence oflder
             if os.path.exists(os.path.join(seq_path, "batches")):
                 seq_path = os.path.join(seq_path, "batches")
+
             for file in os.listdir(seq_path):
                 if limit and len(self.files_list) >= limit:
                     break
                 if file.endswith(".pt") or file.endswith(".npz"):
                     file_path = os.path.join(seq_path, file)
                     self.files_list.append(file_path)
+
                     if self.preload_to_RAM:
                         batch = self._load_batch(file_path)
                         sample = self.pre_process(batch)
@@ -74,6 +96,35 @@ class CustomDataset(abc.ABC, torch.utils.data.Dataset):
                 )
             )
 
+    def _normalize_events(self, events: torch.Tensor, norm_type: str):
+        supported_types = ["min_max_0_1", "min_max_-1_1", "z_score", "z_score_non_zero"]
+        if norm_type not in supported_types:
+            raise Exception(
+                'Normalization type "{}" not supported. You can choose from {}.'.format(
+                    norm_type, supported_types
+                )
+            )
+
+        # Define min and max for minmax
+        e_min = -255
+        e_max = 255
+
+        if norm_type == "min_max_0_1":
+            events = (events - e_min) / (e_max - e_min)
+        elif norm_type == "min_max_-1_1":
+            events = 2 * ((events - e_min) / (e_max - e_min)) - 1
+        elif norm_type == "z_score":
+            events = (events - events.mean()) / events.std()
+        elif norm_type == "z_score_non_zero":
+            nonzero_ev = events != 0
+            num_nonzeros = nonzero_ev.sum()
+            if num_nonzeros > 0:
+                mean = events.sum() / num_nonzeros
+                stddev = np.sqrt((events**2).sum() / num_nonzeros - mean**2)
+                mask = nonzero_ev.astype(np.float32)
+                events = mask * (events - mean) / stddev
+        return events
+
     @abc.abstractmethod
     def pre_process(self, batch):
         pass
@@ -93,8 +144,8 @@ class CEDDataset(CustomDataset):
             out_img = out_img[:h, :w, :]
             events = events[:, :h, :w]
 
-        if self.normalize_events:
-            events = 2 * ((events - (-255)) / (255 - -255)) - 1
+        if self.events_normalization:
+            events = self._normalize_events(events, self.events_normalization)
 
         if self.convert_to_bw:
             in_img = rgb2gray(in_img)
@@ -118,8 +169,8 @@ class DIV2KDataset(CustomDataset):
             out_img = out_img[:h, :w, :]
             events = events[:, :h, :w]
 
-        if self.normalize_events:
-            events = 2 * ((events - (-255)) / (255 - -255)) - 1
+        if self.events_normalization:
+            events = self._normalize_events(events, self.events_normalization)
 
         if self.convert_to_bw:
             out_img = rgb2gray(out_img)
